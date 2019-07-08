7e2af16e58b7a5d01ef523cea69461bacc6b7a0e
Brian R <glowyrm.prime@gmail.com>
2019-07-08 11:59:51 -0500

    fix routing...

--- src/components/portfolio/PortfolioContainer_01.js
+++ src/components/portfolio/PortfolioContainer_01.js
@@ -6,14 +6,14 @@ import src_cnt from '../../../public/portfolio_cnt.png'
 
 export default function PortfolioContainer_01() {
     const ProjectText = {
-        SFI: `My responsibility on this project was to engineer a bespoke CMS, and relevant UI, that allowed the company to keep their current content creation workflow, and integrated within their existing website which was created using Polymer Web Components. 
-        \nThis project required working with Javascript, OAuth, Firebase, and Polymer Web Components. This was a fun challenge that took a lot of work to map out the website event system, and required a solid knowledge of async Javascript`,
-        ellafund: `I was hired to build a Web Extension allowing users to inject marketing scripts while browsing to generate addvertisement revenue for their charity of choice. (JS, and Web Extensions API).
-        \nI worked with the project's designer to implement the mockups he created in InDesign. I built all of the code which required a lot of async javascript and some heavy research in to the Web Extensins API `,
-        tsampi: `I was invited to be a collaborator on the Tsampi Project which was started by my long time friend , and co-developer Tim Watts. The Tsampi project is a federated permissioned blockchain with the goal of creating federated server protocol
+        SFI: `My responsibility on this project was to engineer a bespoke CMS, and relevant UI, that allowed the company to keep their current content creation workflow. The code also had to integrate within their existing website which was created using Polymer Web Components. 
+        \nThis complex challenge required proficiency with OAuth, Firebase, and Polymer Web Components, as well as, a solid knowledge of async Javascript in order to map out the website's multi-directional event system.`,
+        ellafund: `I was hired to build a Web Extension allowing users to inject marketing scripts while browsing to generate advertisement revenue for their charity of choice.
+        \nI worked with the project's designer to implement the mockups he created in InDesign. The code was developed using async javascript and heavy research in to the Web Extensins API.`,
+        tsampi: `I was invited to be a collaborator on the Tsampi Project which was started by my long-time colleague, Tim Watts. The goal of The Tsampi project is to create a federated server protocol using a permissioned blockchain.
         \nMy contribution was creating a server executable script using NodeJS that implemented a cryptographic proof-of-work algorithm.`,
         cnt: `I was hired for work on the CoatandTails e-commerce site to customise purchase options that the backend wouldn't accomodate, and to allow better integration with their marketing campaign.
-        \nI solved the purchase options task by using the Liquid framework's filters and iteration to generate the neccessary templates. I had to make a way for the cart to track the marketing url and store that in that site's database by using vanilla JS for the front-end, and PHP & SQL for the back-end. `
+        \nI solved the purchase options task by using the Liquid framework's filters and iteration to generate the neccessary templates. In order to facilitate tracking and storage of the marketing url, I utilized vanilla JS for the front-end, and PHP & SQL for the back-end. `
     }
     return (
         <section className="page-section bg-primary">
